65ffd883cd090575a19c379a111fb88537dff967
kkulesz <kulesza.konrad@protonmail.com>
2023-05-11 11:40:31 +0200

    WIP correlation function

--- Homework2/implementation_script.py
+++ Homework2/implementation_script.py
@@ -98,14 +98,25 @@ def correlation(img, template):
         an image containing the correlation between image and template gradients.
     """
 
+    # -compute gradient of the image
     img_grad = utils.calcDirectionalGrad(img)
+    img_r, img_c = img_grad.shape
+    # -compute gradient of the template
     template_grad = utils.calcDirectionalGrad(template)
+    tmp_r, tmp_c = template_grad.shape
 
-    # TODO:
-    # -copy template gradient into larger frame
+    # -copy template gradient into larger frame (pad it with zeros?)
+    template_grad_padded = np.pad(template_grad, ((0, img_r-tmp_r), (0, img_c-tmp_c)), constant_values=0)
+
     # -apply a circular shift so the center of the original template is in the
     #   upper left corner
+    template_grad_padded_shifted = utils.circularShift(template_grad_padded, tmp_c//2, tmp_r//2)
+
     # -normalize template
+    template_sum = np.sum(abs(template_grad_padded_shifted))
+    template_grad_padded_shifted_normalized = template_grad_padded_shifted/template_sum
+
+    # TODO:
     # -compute correlation
 
     return np.zeros_like(img)
